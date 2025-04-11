3c1ced8696f418fe06c54e5d1a58c0034ca26710
johantchulacan <johant.chulacan@campusucc.edu.co>
2025-04-11 19:49:15 +0000

    se añade los constructores y calculos

--- actividadevaluativa2/main.java
+++ actividadevaluativa2/main.java
@@ -48,21 +48,65 @@ public class PrecioTotal {
     private double tamanio;
     private double precioBase;
     // Constructores
+
+    public Equipaje(double peso, double tamanio) {
+        this.peso = peso;
+        this.tamanio = tamanio;
+        this.precioBase = PRECIO_BASE;
+    }
+
+    public Equipaje(double precioBase) {
+        this.peso = PESO;
+        this.tamanio = TAMANIO;
+        this.precioBase = precioBase;
+    }
+
+    public Equipaje() {
+        this.peso = PESO;
+        this.tamanio = TAMANIO;
+        this.precioBase = PRECIO_BASE;
+    }
    
     // Metodos
     public double calcularPrecio(){
     return 0.0;
     }
     // getters/setters de ser necesarios
-   }
+
+    public double getPeso() {
+        return peso;
+    }
+
+    public double getTamanio() {
+        return tamanio;
+    }
+
+    public double getPrecioBase() {
+        return precioBase;
+    }
+}
+
    public class Bodega extends Equipaje {
     // Constantes
     private static final double CAPACIDAD = 8.0;
     // Constructores
-   
+    public Bodega(double peso, double tamanio) {
+        super(peso, tamanio);
+    }
+
+    public Bodega(double precioBase) {
+        super(precioBase);
+    }
+
+    public Bodega() {
+        super();
+    }
+
     // Metodos
     public double calcularPrecio(){
+
     // Calculos
+    double precioFinal = precioBase + (peso * tamanio * CAPACIDAD);
     return precioFinal;
     }
     // getters/setters de ser necesarios
@@ -71,10 +115,19 @@ public class PrecioTotal {
     // Constantes
     private final static int TIEMPO = 2;
     // Constructores
-   
+    public Cabina(double precioBase, double tamanio) {
+        super(precioBase);
+        this.tamanio = tamanio;
+    }
+
+    public Cabina() {
+        super();
+    }
+
     // Metodos
     public double calcularPrecio(){
     // Calculos
+    double precioFinal = precioBase + (peso * tamanio * TIEMPO);
     return precioFinal;
     }
     // getters/setters de ser necesarios
